7ce9be1734477f4d7128b395eee8ed78069574fe
Tema Sizyakin <artemsizyakin@gmail.com>
2022-01-24 16:49:52 +0300

    Added ESLint with Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	useTabs: true,
+	tabWidth: 4,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	semi: true,
+	printWidth: 140,
+	parser: 'flow',
+	requirePragma: false,
+	arrowParens: 'avoid',
+};
